fa6b5118b11b63ef8e0751e73c19c82e07211744
Binchang Emmanuel <binchangb@gmail.com>
2022-10-13 15:22:56 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	long x;
 	long long y;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(c));
-	printf("size of an int: %lu byte(s)\n", sizeof(i));
-	printf("size of a long int: %lu byte(s)\n", sizeof(x));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(y));
-	printf("size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(x));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(y));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
